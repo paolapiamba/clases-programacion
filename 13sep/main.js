83b9024f71ed4aa1ebb279d17498c6b559ac3c0b
paola piamba <paolapiamba57@gmail.com>
2022-09-13 14:40:20 -0300

    clases13 de sep

---
+++ 13sep/main.js
@@ -0,0 +1,25 @@
+const number = 17
+let car = "muchos" 
+let red = "veinte"
+let dog = "capitan"
+let eyes = "marrones"
+
+
+//tipo de datos 
+let flag = true //false, dicotonimica
+let number = 1 //number numeros enteros o decimales 
+let tex = " El texto va entre comillas" // string,texto
+
+// arrays, cada elemento tiene un indice y parten desde 0
+let arr = [1,2,3,.8] // arrays
+let arr2 =["texto",[1,2,3,], 4,true ] // arrays
+
+arr[2] // acceder al valor 4
+arr[2]= 5 //asignado un nuevo valor 
+
+// variablename[index]->acceder el valor en un arreglo
+
+let nothing = null 
+let nodef = undefined
+let width //sin un valor asignado, esta variable es "undefined"
+// undefined suele no usarse, ya que javascrip lo hace solo.
