5f2d747c66cc1e756e5e15de915766d53362a797
paola piamba <paolapiamba57@gmail.com>
2022-09-27 13:33:41 -0300

    ciclos

---
+++ 26sep-ciclos/main.js
@@ -0,0 +1,25 @@
+/*
+let n = 1
+do{
+    console.log(n)
+    n = n+1
+}while( n==0||n<=10)
+
+
+let m =10
+do{
+    console.log(m)
+    m=m-1
+}while(m==0  || m>=1)
+
+*/
+const name = "pao"
+let n1=name.length-1
+do{
+    console.log(name[n1])
+    n1=n1-1
+}while(n1>=0)
+
+
+
+
